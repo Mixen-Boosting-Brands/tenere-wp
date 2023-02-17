ff11bfa498d932b2c21adb07938acab68de3b670
Luis Carlos Pando <hey@luiscarlospando.com>
2023-02-17 15:44:03 -0600

    Armar header y footer

--- footer.php
+++ footer.php
@@ -1,34 +1,182 @@
-			<!-- footer -->
-			<footer class="footer" role="contentinfo">
+	<footer id="contacto">
+        <div class="container">
+            <div class="row mb-5">
+                <div class="col-lg-4 order-lg-3 mb-4 mb-lg-0">
+                    <div class="row" data-aos="fade-up" data-aos-duration="1000">
+                        <div class="col-lg-6 col-xl-4 offset-xl-2">
+                            <h1>Menú</h1>
+                        </div>
+                        <div class="col-lg-6">
+                            <nav>
+                                <ul class="list-unstyled">
+                                    <li>
+                                        <a href="#">
+                                            Inicio
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            Nosotros
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            Servicios
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            Blog
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            Podcast
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a class="anchor" href="#contacto">
+                                            Contacto
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            Política
+                                        </a>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 order-lg-2 mb-4 mb-lg-0">
+                    <!-- Aquí se inyecta feedback a usuario vía Ajax -->
+                    <div id="form-messages"></div>
+                    <!-- /Aquí se inyecta feedback a usuario vía Ajax -->
+                    <form data-aos="fade-up" data-aos-duration="1000" action="<?php echo esc_url( get_template_directory_uri() ); ?>/mailer.php" method="POST" class="row g-3 needs-validation contact-form mb-4 mb-lg-0" id="ajax-contact" novalidate>
+                        <div class="row">
+                            <div class="col-md-6 form-floating">
+                                <input type="text" class="form-control shadow-none" id="nombre" name="nombre" placeholder="Nombre*"
+                                    pattern=".{5,50}" required>
+                                <label for="nombre" class="form-label">Nombre*</label>
+                                <div class="valid-feedback">
+                                    ¡Se ve bien!
+                                </div>
+                                <div class="invalid-feedback">
+                                    Por favor introduce tu nombre completo.
+                                </div>
+                            </div>
+                            <div class="col-md-6 form-floating">
+                                <input type="email" class="form-control shadow-none" id="correo" name="correo" placeholder="Email*"
+                                    required>
+                                <label for="correo" class="form-label">Email*</label>
+                                <div class="valid-feedback">
+                                    ¡Se ve bien!
+                                </div>
+                                <div class="invalid-feedback">
+                                    Por favor introduce un correo electrónico válido.
+                                </div>
+                            </div>
+                            <div class="col-md-12 form-floating">
+                                <input type="tel" class="form-control shadow-none" id="telefono" name="telefono" placeholder="Teléfono*"
+                                    required>
+                                <label for="telefono" class="form-label">Teléfono*</label>
+                                <div class="valid-feedback">
+                                    ¡Se ve bien!
+                                </div>
+                                <div class="invalid-feedback">
+                                    Por favor introduce un número de teléfono válido (p. ej: 6141234567).
+                                </div>
+                            </div>
+                            <div class="col-md-12 form-floating mb-4">
+                                <textarea class="form-control shadow-none" id="mensaje" name="mensaje" placeholder="Mensaje*" style="height: 100px;" required></textarea>
+                                <label for="mensaje">Mensaje*</label>
+                                <div class="valid-feedback">
+                                    ¡Se ve bien!
+                                </div>
+                                <div class="invalid-feedback">
+                                    Por favor introduce tu mensaje.
+                                </div>
+                            </div>
+                            <div class="col-md-6 my-auto">
+                                <button type="submit" class="btn btn-primary btn-block"><i class="fa-solid fa-paper-plane"></i> Enviar</button>
+                                <div id="hold-on-a-sec">
+                                    <i id="contact-spinner" class="fas fa-spinner fa-spin"></i> Espera un momento por favor...
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <div class="col-lg-4 order-lg-1">
+                    <div class="row mb-4" data-aos="fade-up" data-aos-duration="1000">
+                        <div class="col">
+                            <h1>Chihuahua</h1>
+                            <ul class="list-unstyled info">
+                                <li>
+                                    <a href="#">
+                                        <i class="fa-solid fa-location-pin"></i>
+                                    </a>
+                                    <address>
+                                        Av. de la Empresa<br>
+                                        Vistas del Sol, Chihuahua, Chih.
+                                    </address>
+                                </li>
+                                <li>
+                                    <a href="tel:+526143970559">
+                                        <i class="fa-solid fa-phone"></i>
+                                    </a>
+                                    <span>(614) 397 0559</span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="row" data-aos="fade-up" data-aos-duration="1000">
+                        <div class="col">
+                            <h1>Síguenos</h1>
+                            <h6 class="text-uppercase">teneremx</h6>
+                            <p></p>
+                            <ul class="list-inline social">
+                                <li class="list-inline-item">
+                                    <a href="https://facebook.com/teneremx" target="_blank">
+                                        <i class="fa-brands fa-facebook-f"></i>
+                                    </a>
+                                </li>
+                                <li class="list-inline-item">
+                                    <a href="https://instagram.com/teneremx" target="_blank">
+                                        <i class="fa-brands fa-instagram"></i>
+                                    </a>
+                                </li>
+                                <li class="list-inline-item">
+                                    <a href="https://tiktok.com/teneremx" target="_blank">
+                                        <i class="fa-brands fa-tiktok"></i>
+                                    </a>
+                                </li>
+                                <li class="list-inline-item">
+                                    <a href="https://linkedin.com/teneremx" target="_blank">
+                                        <i class="fa-brands fa-linkedin"></i>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col text-center" data-aos="fade-in" data-aos-duration="1000" data-aos-delay="500">
+                    <a href="#">
+                        <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/logo.webp" alt="" class="logo img-fluid">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </footer>
 
-				<!-- copyright -->
-			    <?php
-				    printf( '<p class="copyright">' . __( '&copy; %1$s Copyright %2$s. Powered by <a href="%3$s" title="WordPress">WordPress</a> &amp; <a href="%4$s" title="HTML5 Blank">HTML5 Blank</a>.', 'html5blank' ) . '</p>',
-				    	date( 'Y'),
-				    	esc_html( get_bloginfo( 'name' ) ),
-				    	'//wordpress.org',
-				    	'//html5blank.com'
-					);
-				?>
-				<!-- /copyright -->
+    <a href="https://wa.me/52614?text=Hola%20FOO%20BAR,%20necesito%20información." class="whatsapp" target="_blank">
+        <i class="fab fa-whatsapp whatsapp-icon"></i>
+    </a>
 
-			</footer>
-			<!-- /footer -->
+	<?php wp_footer(); ?>
 
-		</div>
-		<!-- /wrapper -->
-
-		<?php wp_footer(); ?>
-
-		<!-- analytics -->
-		<script>
-		(function(f,i,r,e,s,h,l){i['GoogleAnalyticsObject']=s;f[s]=f[s]||function(){
-		(f[s].q=f[s].q||[]).push(arguments)},f[s].l=1*new Date();h=i.createElement(r),
-		l=i.getElementsByTagName(r)[0];h.async=1;h.src=e;l.parentNode.insertBefore(h,l)
-		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-		ga('create', 'UA-XXXXXXXX-XX', 'yourdomain.com');
-		ga('send', 'pageview');
-		</script>
-
-	</body>
+    <script src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/js/app.bundle.js"></script>
+</body>
 </html>
--- header.php
+++ header.php
@@ -1,51 +1,128 @@
-<!doctype html>
-<html <?php language_attributes(); ?> class="no-js">
-	<head>
-		<meta charset="<?php bloginfo( 'charset' ); ?>">
-		<title><?php wp_title( '' ); ?><?php if ( wp_title( '', false ) ) { echo ' : '; } ?><?php bloginfo( 'name' ); ?></title>
+<!DOCTYPE html>
+<html lang="es-MX">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php wp_title( '' ); ?><?php if ( wp_title( '', false ) ) { echo ' : '; } ?><?php bloginfo( 'name' ); ?></title>
 
-		<link href="//www.google-analytics.com" rel="dns-prefetch">
-		<link href="<?php echo esc_url( get_template_directory_uri() ); ?>/img/icons/favicon.ico" rel="shortcut icon">
-		<link href="<?php echo esc_url( get_template_directory_uri() ); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
-		<link rel="alternate" type="application/rss+xml" title="<?php bloginfo( 'name' ); ?>" href="<?php bloginfo( 'rss2_url' ); ?>" />
+	<link href="//www.google-analytics.com" rel="dns-prefetch">
+	<link href="<?php echo esc_url( get_template_directory_uri() ); ?>/img/icons/favicon.ico" rel="shortcut icon">
+	<link href="<?php echo esc_url( get_template_directory_uri() ); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
+	<link rel="alternate" type="application/rss+xml" title="<?php bloginfo( 'name' ); ?>" href="<?php bloginfo( 'rss2_url' ); ?>" />
 
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta name="description" content="<?php bloginfo( 'description' ); ?>">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="<?php bloginfo( 'description' ); ?>">
 
-		<?php wp_head(); ?>
-		<script>
-		// conditionizr.com
-		// configure environment tests
-		conditionizr.config({
-			assets: '<?php echo esc_url( get_template_directory_uri() ); ?>',
-			tests: {}
-		});
-		</script>
+    <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+    <link rel="manifest" href="site.webmanifest">
+    <link rel="mask-icon" href="safari-pinned-tab.svg" color="#131a60">
+    <meta name="msapplication-TileColor" content="#131a60">
+    <meta name="theme-color" content="#131a60">
 
-	</head>
-	<body <?php body_class(); ?>>
+	<?php wp_head(); ?>
 
-		<!-- wrapper -->
-		<div class="wrapper">
+    <link rel="stylesheet" href="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/css/styles.css">
+</head>
+<body>
+    <div id="backdrop"></div>
+    <div class="menu">
+        <a id="cerrar-menu" href="javascript:void(0)">
+            <i class="fas fa-times"></i>
+        </a>
+        <div class="menu-contenido">
+            <a class="anchor" id="btn-logo" href="<?php echo esc_url( home_url() ); ?>">
+                <img class="logo img-fluid" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/logo.webp" alt="">
+            </a>
+            <nav>
+                <ul class="list-unstyled">
+                    <li>
+                        <a id="btn-nav-1" href="<?php echo esc_url( home_url() ); ?>">Inicio</a>
+                    </li>
+                    <li>
+                        <a <?php if ( is_home() ): ?>class="anchor"<?php endif; ?> id="btn-nav-2" href="<?php if ( !is_home() ): ?><?php echo esc_url( home_url() ); ?>#nosotros<?php else: ?>#nosotros<?php endif; ?>">Nosotros</a>
+                    </li>
+                    <li>
+                        <a <?php if ( is_home() ): ?>class="anchor"<?php endif; ?> id="btn-nav-3" href="<?php if ( !is_home() ): ?><?php echo esc_url( home_url() ); ?>#nuestros-servicios<?php else: ?>#nuestros-servicios<?php endif; ?>">Servicios</a>
+                    </li>
+                </ul>
+            </nav>
+            <a href="https://calendly.com/teneremx" class="anchor btn btn-primary" id="btn-contacto">
+				<i class="fa-solid fa-calendar-days"></i> 
+				Agenda una cita
+			</a>
+            <div id="social">
+                <ul class="list-inline">
+                    <li class="list-inline-item">
+                        <a href="https://facebook.com/teneremx" target="_blank">
+                            <i class="fab fa-facebook-square"></i>
+                        </a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="https://instagram.com/teneremx" target="_blank">
+                            <i class="fab fa-instagram"></i>
+                        </a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="https://tiktok.com/teneremx" target="_blank">
+                            <i class="fab fa-tiktok"></i>
+                        </a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="https://linkedin.com/teneremx" target="_blank">
+                            <i class="fab fa-linkedin"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div id="contacto-menu">
+                <ul class="list-unstyled">
+                    <li>
+                        <i class="far fa-envelope"></i> <a href="mailto:cmail@domain.com">mail@domain.com</a>
+                    </li>
+                    <li>
+                        <i class="fas fa-phone"></i> <a href="tel:+526141234567">(614) 123 4567</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
 
-			<!-- header -->
-			<header class="header clear" role="banner">
-
-					<!-- logo -->
-					<div class="logo">
-						<a href="<?php echo esc_url( home_url() ); ?>">
-							<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
-							<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/logo.svg" alt="Logo" class="logo-img">
-						</a>
-					</div>
-					<!-- /logo -->
-
-					<!-- nav -->
-					<nav class="nav" role="navigation">
-						<?php html5blank_nav(); ?>
-					</nav>
-					<!-- /nav -->
-
-			</header>
-			<!-- /header -->
+    <header id="navbar">
+        <div class="container">
+            <div class="row">
+                <div class="col-6 col-lg-3 my-auto">
+                    <a href="#">
+                        <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/logo.webp" alt="" class="logo img-fluid" id="logo-navbar">
+                    </a>
+                </div>
+                <div class="col-lg-6 my-auto text-center d-none d-lg-block">
+                    <nav>
+                        <ul class="list-inline">
+                            <li class="list-inline-item">
+                                <a href="<?php echo esc_url( home_url() ); ?>">Inicio</a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a <?php if ( is_home() ): ?>class="anchor"<?php endif; ?> href="<?php if ( !is_home() ): ?><?php echo esc_url( home_url() ); ?>#nosotros<?php else: ?>#nosotros<?php endif; ?>">Nosotros</a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a <?php if ( is_home() ): ?>class="anchor"<?php endif; ?> href="<?php if ( !is_home() ): ?><?php echo esc_url( home_url() ); ?>#nuestros-servicios<?php else: ?>#nuestros-servicios<?php endif; ?>">Servicios</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+                <div class="col-6 col-lg-3 my-auto text-end">
+                    <a id="mburger" class="d-lg-none" href="javascript:void(0)">
+                        <i class="fas fa-bars"></i>
+                    </a>
+                    <a href="https://calendly.com/teneremx" class="btn btn-primary d-none d-lg-block" target="_blank">
+                        <i class="fa-solid fa-calendar-days"></i> 
+                        Agenda una cita
+                    </a>
+                </div>
+            </div>
+        </div>
+    </header>
