8b1475552f1d986954ab35a0af8d028f246b628c
Luis Carlos Pando <hey@luiscarlospando.com>
2023-02-20 15:55:42 -0600

    Terminar vistas faltantes

--- tag.php
+++ tag.php
@@ -1,19 +1,45 @@
 <?php get_header(); ?>
 
-	<main role="main" aria-label="Content">
-		<!-- section -->
-		<section>
+<section class="interna blog">
+	<div class="container">
+		<div class="row mb-5">
+			<div class="col-12 text-center">
+				<h1><?php esc_html_e( 'Archivo de etiquetas: ', 'html5blank' ); echo single_tag_title( '', false ); ?></h1>
+			</div>
+		</div>
+	<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+		<div class="row post mb-4">
+			<div class="col-5 col-sm-5 my-auto text-center">
+				<a href="<?php the_permalink(); ?>">
+					<figure>
+						<?php the_post_thumbnail('large', array('class' => 'img-fluid')); ?>
+					</figure>
+				</a>
+			</div>
+			<div class="col-7 col-sm-7 my-auto">
+				<div class="row mb-3">
+					<div class="col">
+						<a href="<?php the_permalink(); ?>">
+							<h2><?php the_title(); ?></h2>
+						</a>
+						<?php html5wp_excerpt( 'html5wp_index' ); ?>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col">
+						<a href="<?php the_permalink(); ?>" class="btn btn-secondary rounded-pill">
+							Leer más
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	<?php endwhile; ?>
+	<?php else : ?>
+	<?php endif; ?>
 
-			<h1><?php esc_html_e( 'Tag Archive: ', 'html5blank' ); echo single_tag_title( '', false ); ?></h1>
-
-			<?php get_template_part( 'loop' ); ?>
-
-			<?php get_template_part( 'pagination' ); ?>
-
-		</section>
-		<!-- /section -->
-	</main>
-
-<?php get_sidebar(); ?>
+		<?php get_template_part( 'pagination' ); ?>	
+	</div>
+</section>
 
 <?php get_footer(); ?>
