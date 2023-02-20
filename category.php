e88e5d34faf1e44178351bf3b46c206937a58803
Luis Carlos Pando <hey@luiscarlospando.com>
2023-02-20 15:40:41 -0600

    Restructurar vista categorías

--- category.php
+++ category.php
@@ -2,10 +2,20 @@
 
 <section class="interna">
 	<div class="container">
-	<?php if (have_posts()): while (have_posts()) : the_post(); ?>
 		<div class="row">
 			<div class="col-12">
 				<h1><?php esc_html_e( '', 'html5blank' ); single_cat_title(); ?></h1>
+			</div>
+		</div>
+	<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+		<div class="row">
+			<div class="col-4 my-auto">
+				<figure>
+					<?php the_post_thumbnail('large', array('class' => 'img-fluid')); ?>
+				</figure>
+			</div>
+			<div class="col-6 my-auto">
+				<h1><?php the_title(); ?></h1>
 				<ul class="list-inline">
 					<li class="list-inline-item">
 						<p><?php esc_html_e( 'Por ', 'html5blank' ); the_author(); ?></p>
@@ -14,15 +24,6 @@
 						<p><?php the_date(); ?> <?php the_time(); ?></p>
 					</li>
 				</ul>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-4 my-auto">
-				<figure>
-					<?php the_post_thumbnail('large', array('class' => 'img-fluid')); ?>
-				</figure>
-			</div>
-			<div class="col-6 my-auto">
 				<?php html5wp_excerpt( 'html5wp_index' ); ?>
 				<?php edit_post_link(); ?>
 			</div>
