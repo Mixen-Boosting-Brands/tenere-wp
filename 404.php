37aab7901199d0c19d09d577788634234aafa4ae
Luis Carlos Pando <hey@luiscarlospando.com>
2023-02-20 13:44:06 -0600

    Modificar vista error 404

--- 404.php
+++ 404.php
@@ -1,24 +1,21 @@
 <?php get_header(); ?>
 
-	<main role="main" aria-label="Content">
-		<!-- section -->
-		<section>
+<?php if ( have_posts()) : while ( have_posts() ) : the_post(); ?>
 
-			<!-- article -->
-			<article id="post-404">
+	<section class="interna" data-aos="fade-up" data-aos-duration="1000">
+		<div class="container">
+			<div class="row">
+				<div class="col-12">
+					<h1><?php esc_html_e( 'Error 404', 'html5blank' ); ?></h1>
+					<p>No se encontró a página solicitada. <a href="<?php echo esc_url( home_url() ); ?>">Volver al inicio</a>.</p>
+					<?php edit_post_link(); ?>
+				</div>
+			</div>
+		</div>
+	</section>
 
-				<h1><?php esc_html_e( 'Page not found', 'html5blank' ); ?></h1>
-				<h2>
-					<a href="<?php echo esc_url( home_url() ); ?>"><?php esc_html_e( 'Return home?', 'html5blank' ); ?></a>
-				</h2>
-
-			</article>
-			<!-- /article -->
-
-		</section>
-		<!-- /section -->
-	</main>
-
-<?php get_sidebar(); ?>
+<?php endwhile; ?>
+<?php else : ?>
+<?php endif; ?>
 
 <?php get_footer(); ?>
