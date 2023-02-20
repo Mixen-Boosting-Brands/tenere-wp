c4208a6187ff26b869fef1e7e7ce344cc871fadf
Luis Carlos Pando <hey@luiscarlospando.com>
2023-02-20 13:42:38 -0600

    Corregir vistas internas

--- page.php
+++ page.php
@@ -3,11 +3,15 @@
 <?php if ( have_posts()) : while ( have_posts() ) : the_post(); ?>
 
 	<section class="interna" data-aos="fade-up" data-aos-duration="1000">
-		<h1><?php the_title(); ?></h1>
-
-		<?php the_content(); ?>
-
-		<?php edit_post_link(); ?>
+		<div class="container">
+			<div class="row">
+				<div class="col-12">
+					<h1><?php the_title(); ?></h1>
+					<?php the_content(); ?>
+					<?php edit_post_link(); ?>
+				</div>
+			</div>
+		</div>
 	</section>
 
 <?php endwhile; ?>
--- single.php
+++ single.php
@@ -3,22 +3,24 @@
 <?php if ( have_posts()) : while ( have_posts() ) : the_post(); ?>
 
 	<section class="interna" data-aos="fade-up" data-aos-duration="1000">
-		<h1><?php the_title(); ?></h1>
-
-		<ul class="list-inline">
-			<li class="list-inline-item">
-				<p><?php esc_html_e( 'Por ', 'html5blank' ); the_author(); ?></p>
-			</li>
-			<li class="list-inline-item">
-				<p><?php the_date(); ?> <?php the_time(); ?></p>
-			</li>
-		</ul>
-
-		<?php the_content(); ?>
-
-		<?php comments_template( '', true ); // Remove if you don't want comments. ?>
-
-		<?php edit_post_link(); ?>
+		<div class="container">
+			<div class="row">
+				<div class="col-12">
+					<h1><?php the_title(); ?></h1>
+					<ul class="list-inline">
+						<li class="list-inline-item">
+							<p><?php esc_html_e( 'Por ', 'html5blank' ); the_author(); ?></p>
+						</li>
+						<li class="list-inline-item">
+							<p><?php the_date(); ?> <?php the_time(); ?></p>
+						</li>
+					</ul>
+					<?php the_content(); ?>
+					<?php comments_template( '', true ); // Remove if you don't want comments. ?>
+					<?php edit_post_link(); ?>
+				</div>
+			</div>
+		</div>
 	</section>
 
 <?php endwhile; ?>
