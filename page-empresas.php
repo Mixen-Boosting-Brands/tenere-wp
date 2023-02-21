50b32e16724a7bcc538f10eb63b5475076661a3d
Luis Carlos Pando <hey@luiscarlospando.com>
2023-02-21 15:30:32 -0600

    Agregar vistas faltantes

---
+++ page-empresas.php
@@ -0,0 +1,94 @@
+<?php get_header(); ?>
+
+	<section id="servicios" class="descanso-alt">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 mb-lg-0">
+                    <div class="card" data-aos="fade-up" data-aos-duration="1000">
+                        <div id="icono-hombre-clave" class="icono"> 
+                            <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/iconos/servicios/empresas/ico-hombre-clave.png" alt="" class="img-fluid">
+                        </div>
+                        <div class="card-body">
+                            <h1>Hombre clave</h1>
+                            <p>
+                                Protege la vida  de personas clave en tu negocio, crea un ahorro para su retiro y blínda su productividad ante una invalidez.
+                            </p>
+                        </div>
+                        <a href="#" class="btn btn-primary rounded-pill">
+                            Cotizar
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 mb-lg-0">
+                    <div class="card" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="250">
+                        <div id="icono-gastos-medicos-empresariales" class="icono"> 
+                            <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/iconos/servicios/empresas/ico-gastos-medicos-empresariales.png" alt="" class="img-fluid">
+                        </div>
+                        <div class="card-body">
+                            <h1>Gastos médicos empresariales</h1>
+                            <p>
+                                Dale a tus empleados la mejor atención, disminuye tu rotación y protege a los tuyos.
+                            </p>
+                        </div>
+                        <a href="#" class="btn btn-primary rounded-pill">
+                            Cotizar
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 mb-lg-0">
+                    <div class="card" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="500">
+                        <div id="icono-vida-empresarial" class="icono"> 
+                            <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/iconos/servicios/empresas/ico-vida-empresarial.png" alt="" class="img-fluid">
+                        </div>
+                        <div class="card-body">
+                            <h1>Vida empresarial</h1>
+                            <p>
+                                Tus colaboradores son importantes, dales la tranquilidad de que los estás protegiendo. 
+                            </p>
+                        </div>
+                        <a href="#" class="btn btn-primary rounded-pill">
+                            Cotizar
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 mb-lg-0">
+                    <div class="card" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="750">
+                        <div id="icono-seguro-comercial" class="icono"> 
+                            <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/iconos/servicios/empresas/ico-seguro-comercio.png" alt="" class="img-fluid">
+                        </div>
+                        <div class="card-body">
+                            <h1>Seguro comercio</h1>
+                            <p>
+                                No pongas en riesgo tu patrimonio blíndate ante cualquier accidente. ¡Incluye tu póliza de Responsabilidad Civil!
+                            </p>
+                        </div>
+                        <a href="#" class="btn btn-primary rounded-pill">
+                            Cotizar
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="row banner">
+                <div class="col-lg-3 mb-4 mb-lg-0 my-auto">
+                    <figure>
+                        <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/logo-sencillo.png" alt="" class="img-fluid" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="1000">
+                    </figure>
+                </div>
+                <div class="col-lg-9 my-auto" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="1250">
+                    <h6 class="text-uppercase">
+                        En Tenere
+                    </h6>
+                    <h1 class="text-uppercase">
+                        ¡Te ayudamos a crear<br>
+                        estrategias fiscales, mientras<br>
+                        aumentas tu patrimonio!
+                    </h1>
+                </div>
+            </div>
+        </div>
+    </section>
+
+<?php get_footer(); ?>
