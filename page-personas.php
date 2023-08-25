5a7d6af8f0562a5c0bc12f9886f61421485afb88
Luis Carlos Pando <hey@luiscarlospando.com>
2023-08-25 15:25:30 -0600

    Armar esqueleto de carrusel de servicios

--- page-personas.php
+++ page-personas.php
@@ -1,141 +1,86 @@
 <?php get_header(); ?>
 
-	<section id="servicios" class="descanso-alt">
-        <div class="container">
+    <section class="carrusel-servicios">
+        <div class="container-fluid">
             <div class="row">
-                <div class="col-lg-3 mb-lg-0">
-                    <div class="card" data-aos="fade-up" data-aos-duration="1000">
-                        <div id="icono-gastos-medicos" class="icono">
-                            <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/iconos/servicios/personas/ico-gastos-medicos.png" alt="" class="img-fluid">
+                <div class="col-12">
+                    <div id="carrusel-servicios" class="carousel slide">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <div class="row">
+                                    <div class="col-lg-6 my-auto">
+                                        <div class="contenedor-info contenedor-blanco">
+                                            <div class="row">
+                                                <div class="col-4 text-center my-auto">
+                                                    <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/iconos/servicios/personas/ico-gastos-medicos.png" alt="" class="img-fluid">
+                                                </div>
+                                                <div class="col-8 my-auto">
+                                                    <h1>Gastos médicos</h1>
+                                                    <p>
+                                                        Protege a ti y a tu familia ante accidentes y enfermedades. Además de obtener beneficios exclusivos en tu maternidad, servicio dental, asistencia psicologíca y más.
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6 text-center">
+                                        <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/servicios/personas/gastos-medicos.png" alt="" class="img-fluid">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <div class="row">
+                                    <div class="col-lg-6 my-auto">
+                                        <div class="contenedor-info contenedor-blanco">
+                                            <div class="row">
+                                                <div class="col-4 text-center my-auto">
+                                                    <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/iconos/servicios/personas/ico-gastos-medicos.png" alt="" class="img-fluid">
+                                                </div>
+                                                <div class="col-8 my-auto">
+                                                    <h1>Gastos médicos</h1>
+                                                    <p>
+                                                        Protege a ti y a tu familia ante accidentes y enfermedades. Además de obtener beneficios exclusivos en tu maternidad, servicio dental, asistencia psicologíca y más.
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6 text-center">
+                                        <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/servicios/personas/gastos-medicos.png" alt="" class="img-fluid">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <div class="row">
+                                    <div class="col-lg-6 my-auto">
+                                        <div class="contenedor-info contenedor-blanco">
+                                            <div class="row">
+                                                <div class="col-4 text-center my-auto">
+                                                    <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/iconos/servicios/personas/ico-gastos-medicos.png" alt="" class="img-fluid">
+                                                </div>
+                                                <div class="col-8 my-auto">
+                                                    <h1>Gastos médicos</h1>
+                                                    <p>
+                                                        Protege a ti y a tu familia ante accidentes y enfermedades. Además de obtener beneficios exclusivos en tu maternidad, servicio dental, asistencia psicologíca y más.
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6 text-center">
+                                        <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/servicios/personas/gastos-medicos.png" alt="" class="img-fluid">
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div class="card-body">
-                            <h1>Gastos médicos</h1>
-                            <p>
-                                Protege a ti y a tu familia ante accidentes y enfermedades. Además de obtener beneficios exclusivos en tu maternidad, servicio dental, asistencia psicologíca y más.
-                            </p>
-                        </div>
-                        <a href="https://forms.gle/4pznpkTJ2gwdmc9i7" class="btn btn-primary rounded-pill" target="_blank">
-                            Cotizar
-                        </a>
-                    </div>
-                </div>
-
-                <div class="col-lg-3 mb-lg-0">
-                    <div class="card" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="250">
-                        <div id="icono-vida" class="icono"> 
-                            <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/iconos/servicios/personas/ico-vida.png" alt="" class="img-fluid">
-                        </div>
-                        <div class="card-body">
-                            <h1>Vida</h1>
-                            <p>
-                                Deja protegidos a los que amas y crea una estrategia para tu legado.
-                            </p>
-                        </div>
-                        <a href="https://forms.gle/4pznpkTJ2gwdmc9i7" class="btn btn-primary rounded-pill" target="_blank">
-                            Cotizar
-                        </a>
-                    </div>
-                </div>
-
-                <div class="col-lg-3 mb-lg-0">
-                    <div class="card" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="500">
-                        <div id="icono-invalidez" class="icono"> 
-                            <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/iconos/servicios/personas/ico-invalidez.png" alt="" class="img-fluid">
-                        </div>
-                        <div class="card-body">
-                            <h1>Invalidez</h1>
-                            <p>
-                                ¿Que sucedería si hoy no pudieras continuar trabajando? Blinda tus planes y tu patrimonio.
-                            </p>
-                        </div>
-                        <a href="https://forms.gle/4pznpkTJ2gwdmc9i7" class="btn btn-primary rounded-pill" target="_blank">
-                            Cotizar
-                        </a>
-                    </div>
-                </div>
-
-                <div class="col-lg-3 mb-lg-0">
-                    <div class="card" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="750">
-                        <div id="icono-educacion" class="icono"> 
-                            <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/iconos/servicios/personas/ico-educacion.png" alt="" class="img-fluid">
-                        </div>
-                        <div class="card-body">
-                            <h1>Educación</h1>
-                            <p>
-                                ¿Universidad en el extranjero? ¿Que quieres para el futuro de tus hijos? Estamos seguros que lo mejor. Planea la educación de tus hijos. Dales la universidad de primera que se merecen, podemos ayudarte a hacerlo realidad.
-                            </p>
-                        </div>
-                        <a href="https://forms.gle/4pznpkTJ2gwdmc9i7" class="btn btn-primary rounded-pill" target="_blank">
-                            Cotizar
-                        </a>
-                    </div>
-                </div>
-
-                <div class="col-lg-3 mb-lg-0">
-                    <div class="card" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="1000">
-                        <div id="icono-retiro" class="icono"> 
-                            <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/iconos/servicios/personas/ico-retiro.png" alt="" class="img-fluid">
-                        </div>
-                        <div class="card-body">
-                            <h1>Retiro</h1>
-                            <p>
-                                Estrategias de retiro para que vivas esa etapa tal como la sueñas, adaptadas a tus necesidades.
-                            </p>
-                        </div>
-                        <a href="https://forms.gle/4pznpkTJ2gwdmc9i7" class="btn btn-primary rounded-pill" target="_blank">
-                            Cotizar
-                        </a>
-                    </div>
-                </div>
-
-                <div class="col-lg-3 mb-lg-0">
-                    <div class="card" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="1250">
-                        <div id="icono-ahorros" class="icono"> 
-                            <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/iconos/servicios/personas/ico-ahorros.png" alt="" class="img-fluid">
-                        </div>
-                        <div class="card-body">
-                            <h1>Ahorros</h1>
-                            <p>
-                                Platicame tus metas y creamos un plan para lograrlas.
-                            </p>
-                        </div>
-                        <a href="https://forms.gle/4pznpkTJ2gwdmc9i7" class="btn btn-primary rounded-pill" target="_blank">
-                            Cotizar
-                        </a>
-                    </div>
-                </div>
-
-                <div class="col-lg-3 mb-lg-0">
-                    <div class="card" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="1500">
-                        <div id="icono-hogar" class="icono"> 
-                            <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/iconos/servicios/personas/ico-hogar.png" alt="" class="img-fluid">
-                        </div>
-                        <div class="card-body">
-                            <h1>Hogar</h1>
-                            <p>
-                                Protege tu patrimonio ante cualquier eventualidad. Desde una caida accidental de tu computadora hasta un incendio fatal.
-                            </p>
-                        </div>
-                        <a href="https://forms.gle/4pznpkTJ2gwdmc9i7" class="btn btn-primary rounded-pill" target="_blank">
-                            Cotizar
-                        </a>
-                    </div>
-                </div>
-
-                <div class="col-lg-3 mb-lg-0">
-                    <div class="card" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="1750">
-                        <div id="icono-inversion" class="icono"> 
-                            <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/iconos/servicios/personas/ico-inversion.png" alt="" class="img-fluid">
-                        </div>
-                        <div class="card-body">
-                            <h1>Inversión</h1>
-                            <p>
-                                Alternativas de inversión personalizadas según tus metas, asesoría en gestión de riesgo. Maximícemos tus ganancias sin poner en riesgo tu patrimonio.
-                            </p>
-                        </div>
-                        <a href="https://forms.gle/4pznpkTJ2gwdmc9i7" class="btn btn-primary rounded-pill" target="_blank">
-                            Cotizar
-                        </a>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carrusel-servicios" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carrusel-servicios" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
                     </div>
                 </div>
             </div>
