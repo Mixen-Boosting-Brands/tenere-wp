e38ade69db85522714e06f846612495748c2eb71
Luis Carlos Pando <hey@luiscarlospando.com>
2023-08-25 17:34:23 -0600

    Armar carrusel de personas

--- page-personas.php
+++ page-personas.php
@@ -28,47 +28,157 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="carousel-item">
+                            <div class="carousel-item vida-item">
                                 <div class="row">
                                     <div class="col-lg-6 my-auto">
                                         <div class="contenedor-info contenedor-blanco">
                                             <div class="row">
                                                 <div class="col-4 text-center my-auto">
-                                                    <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/iconos/servicios/personas/ico-gastos-medicos.png" alt="" class="img-fluid">
+                                                    <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/iconos/servicios/personas/ico-vida-dark.png" alt="" class="img-fluid">
                                                 </div>
                                                 <div class="col-8 my-auto">
-                                                    <h1>Gastos médicos</h1>
+                                                    <h1>Vida</h1>
                                                     <p>
-                                                        Protege a ti y a tu familia ante accidentes y enfermedades. Además de obtener beneficios exclusivos en tu maternidad, servicio dental, asistencia psicologíca y más.
+                                                        Deja protegidos a los que amas y crea una estrategia para tu legado.
                                                     </p>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
                                     <div class="col-lg-6 my-auto text-center">
-                                        <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/servicios/personas/gastos-medicos.png" alt="" class="img-fluid">
+                                        <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/servicios/personas/vida.png" alt="" class="img-fluid">
                                     </div>
                                 </div>
                             </div>
-                            <div class="carousel-item">
+                            <div class="carousel-item invalidez-item">
                                 <div class="row">
                                     <div class="col-lg-6 my-auto">
                                         <div class="contenedor-info contenedor-blanco">
                                             <div class="row">
                                                 <div class="col-4 text-center my-auto">
-                                                    <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/iconos/servicios/personas/ico-gastos-medicos.png" alt="" class="img-fluid">
+                                                    <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/iconos/servicios/personas/ico-invalidez.png" alt="" class="img-fluid">
                                                 </div>
                                                 <div class="col-8 my-auto">
-                                                    <h1>Gastos médicos</h1>
+                                                    <h1>Invalidez</h1>
                                                     <p>
-                                                        Protege a ti y a tu familia ante accidentes y enfermedades. Además de obtener beneficios exclusivos en tu maternidad, servicio dental, asistencia psicologíca y más.
+                                                        ¿Que sucedería si hoy no pudieras continuar trabajando? Blinda tus planes y tu patrimonio.
                                                     </p>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
                                     <div class="col-lg-6 my-auto text-center">
-                                        <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/servicios/personas/gastos-medicos.png" alt="" class="img-fluid">
+                                        <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/servicios/personas/invalidez.png" alt="" class="img-fluid">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="carousel-item educacion-item">
+                                <div class="row">
+                                    <div class="col-lg-6 my-auto">
+                                        <div class="contenedor-info contenedor-blanco">
+                                            <div class="row">
+                                                <div class="col-4 text-center my-auto">
+                                                    <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/iconos/servicios/personas/ico-educacion.png" alt="" class="img-fluid">
+                                                </div>
+                                                <div class="col-8 my-auto">
+                                                    <h1>Educación</h1>
+                                                    <p>
+                                                        ¿Universidad en el extranjero? ¿Que quieres para el futuro de tus hijos? Estamos seguros que lo mejor. Planea la educación de tus hijos. Dales la universidad de primera que se merecen, podemos ayudarte a hacerlo realidad.
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6 my-auto text-center">
+                                        <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/servicios/personas/educacion.png" alt="" class="img-fluid">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="carousel-item retiro-item">
+                                <div class="row">
+                                    <div class="col-lg-6 my-auto">
+                                        <div class="contenedor-info contenedor-blanco">
+                                            <div class="row">
+                                                <div class="col-4 text-center my-auto">
+                                                    <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/iconos/servicios/personas/ico-retiro-dark.png" alt="" class="img-fluid">
+                                                </div>
+                                                <div class="col-8 my-auto">
+                                                    <h1>Retiro</h1>
+                                                    <p>
+                                                        Estrategias de retiro para que vivas esa etapa tal como la sueñas, adaptadas a tus necesidades.
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6 my-auto text-center">
+                                        <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/servicios/personas/retiro.png" alt="" class="img-fluid">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="carousel-item ahorros-item">
+                                <div class="row">
+                                    <div class="col-lg-6 my-auto">
+                                        <div class="contenedor-info contenedor-blanco">
+                                            <div class="row">
+                                                <div class="col-4 text-center my-auto">
+                                                    <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/iconos/servicios/personas/ico-ahorros.png" alt="" class="img-fluid">
+                                                </div>
+                                                <div class="col-8 my-auto">
+                                                    <h1>Ahorros</h1>
+                                                    <p>
+                                                        Platícame tus metas y creamos un plan para lograrlas.
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6 my-auto text-center">
+                                        <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/servicios/personas/ahorros.png" alt="" class="img-fluid">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="carousel-item hogar-item">
+                                <div class="row">
+                                    <div class="col-lg-6 my-auto">
+                                        <div class="contenedor-info contenedor-blanco">
+                                            <div class="row">
+                                                <div class="col-4 text-center my-auto">
+                                                    <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/iconos/servicios/personas/ico-hogar-dark.png" alt="" class="img-fluid">
+                                                </div>
+                                                <div class="col-8 my-auto">
+                                                    <h1>Hogar</h1>
+                                                    <p>
+                                                        Protege tu patrimonio ante cualquier eventualidad. Desde una caida accidental de tu computadora hasta un incendio fatal.
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6 my-auto text-center">
+                                        <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/servicios/personas/hogar.png" alt="" class="img-fluid">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="carousel-item inversion-item">
+                                <div class="row">
+                                    <div class="col-lg-6 my-auto">
+                                        <div class="contenedor-info contenedor-blanco">
+                                            <div class="row">
+                                                <div class="col-4 text-center my-auto">
+                                                    <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/iconos/servicios/personas/ico-inversion.png" alt="" class="img-fluid">
+                                                </div>
+                                                <div class="col-8 my-auto">
+                                                    <h1>Inversión</h1>
+                                                    <p>
+                                                        Alternativas de inversión personalizadas según tus metas, asesoría en gestión de riesgo. Maximícemos tus ganancias sin poner en riesgo tu patrimonio.
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6 my-auto text-center">
+                                        <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/servicios/personas/inversion.png" alt="" class="img-fluid">
                                     </div>
                                 </div>
                             </div>
