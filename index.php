1e5b07ba196b858e095ff26999a3c5bbc5544846
Luis Carlos Pando <hey@luiscarlospando.com>
2023-12-08 11:20:18 -0600

    Modificar carrusel

--- index.php
+++ index.php
@@ -82,7 +82,7 @@
             <div class="carousel-indicators">
                 <button type="button" data-bs-target="#carrusel-home" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
                 <button type="button" data-bs-target="#carrusel-home" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#carrusel-home" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                <!-- button type="button" data-bs-target="#carrusel-home" data-bs-slide-to="2" aria-label="Slide 3"></button -->
             </div>
             <button class="carousel-control-prev" type="button" data-bs-target="#carrusel-home" data-bs-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
